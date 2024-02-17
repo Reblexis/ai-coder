54661bb0b5c669677152ae0ae990d2e5faa4ba92
Viktor Cihal <viktor.cihal@gmail.com>
2024-02-17 21:47:39 +0100

    Create basic structure

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,9 @@
 mod readme_writer;
 mod lm_interface;
 
-use std::env;
 
-use rust_gpt::RequestBuilder;
-use rust_gpt::ChatModel;
-use rust_gpt::chat::*;
-use rust_gpt::SendRequest;
 
 #[tokio::main]
 async fn main() {
-    let openai_api_key = env::var("OPENAI_API_KEY").unwrap();
-    let request = rust_gpt::RequestBuilder::new(ChatModel::Gpt35Turbo, openai_api_key)
-        .messages(vec![ChatMessage{
-            role: Role::User,
-            content: Some("Create a readme.md for a checkers game (project) made in Rust. This readme will contain the files which the project is composed of and what each file does. Also it will describe in detail, how the project works as a whole and how can a user interact with it.".to_string()),
-        }]).
-        build_chat();
 
-    let result = request.send().await.unwrap();
-    let response = result.choices[0].message.content.as_ref().unwrap();
-    println!("{response}");
 }

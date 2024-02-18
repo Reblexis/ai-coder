8233c62d8e6e21c0f85ba9ce16a730eb1de26beb
Viktor Cihal <viktor.cihal@gmail.com>
2024-02-18 11:16:56 +0100

    Add LM interface

--- src/lm_interface.rs
+++ src/lm_interface.rs
@@ -4,26 +4,27 @@ use std::env;
 use rust_gpt::{RequestBuilder, ChatModel, SendRequest, chat::*};
 struct LMInterface{
     openai_api_key: String,
+    messages: Vec<ChatMessage>,
 }
 impl LMInterface{
     pub fn new()->Self{
         let openai_api_key = env::var("OPENAI_API_KEY").unwrap();
         LMInterface{
             openai_api_key,
+            messages: vec![],
         }
     }
-    pub async fn send_message(&self, message: &str) -> Result<String, Box<dyn std::error::Error>> {
-        let request = rust_gpt::RequestBuilder::new(ChatModel::Gpt35Turbo, self.openai_api_key.clone())
-            .messages(vec![ChatMessage{
-                role: Role::User,
-                content: Some("Create a readme.md for a checkers game (project) made in Rust. This readme will contain the files which the project is composed of and what each file does. Also it will describe in detail, how the project works as a whole and how can a user interact with it.".to_string()),
-            }]).
-            build_chat();
+    pub async fn send_message(&mut self, message: &str) -> Result<String, Box<dyn std::error::Error>> {
+        self.messages.push(ChatMessage {
+            role: Role::User,
+            content: Some(message.to_string()),
+        });
 
+        let request = RequestBuilder::new(ChatModel::Gpt35Turbo, self.openai_api_key.clone())
+            .messages(self.messages.clone())
+            .build_chat();
         let result = request.send().await.unwrap();
         let response = result.choices[0].message.content.as_ref().unwrap();
-        println!("{response}");
-
         Ok(response.clone())
     }
 }
